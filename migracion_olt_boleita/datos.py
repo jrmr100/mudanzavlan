3d0c3afc5ed656b1d86e3d06a2359f8bdd6b3f33
“jrmr100” <jrmr100@gmail.com>
2023-12-05 14:45:08 -0400

    modificaciones plantillas boleita

--- migracion_olt_boleita/datos.py
+++ migracion_olt_boleita/datos.py
@@ -12,4 +12,4 @@ interface_vieja = """interface TenGigabitEthernet0/3/0.3313
  ip address 172.23.238.77 255.255.255.252
  service-policy input 150M
  service-policy output 150M"""
-rutas_ip = """ip route 190.94.224.72 255.255.255.252 172.23.238.78 name GPON-V3313-TUNO-LASER-LAS-MERCE"""
+rutas_ip = """ip route 190.94.224.72 255.255.255.252 172.23.238.78 name GPON-VL3313-TUNO-LASER-LAS-MERCE"""
