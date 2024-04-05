00ea6b4123a00b1230085594f81f30836da40dbc
jrmr100 <jrmr100@gmail.com>
2024-04-05 16:27:03 -0400

    feat: migracion router 2net

--- migracion_router2net/conversor.py
+++ migracion_router2net/conversor.py
@@ -33,6 +33,45 @@ def reemplazo_valores(tipo_valor, index_lista, linea):
         return linea_nueva
 
 
+def convertir_mask(mask):
+    dicc_mask = {"255.255.255.255": "/32",
+                 "255.255.255.254": "/31",
+                 "255.255.255.252": "/30",
+                 "255.255.255.248": "/29",
+                 "255.255.255.240": "/28",
+                 "255.255.255.224": "/27",
+                 "255.255.255.192": "/26",
+                 "255.255.255.128": "/25",
+                 "255.255.255.0": "/24",
+                 "255.255.254.0": "/23",
+                 "255.255.252.0": "/22",
+                 "255.255.248.0": "/21",
+                 "255.255.240.0": "/20",
+                 "255.255.224.0": "/19",
+                 "255.255.192.0": "/18",
+                 "255.255.128.0": "/17",
+                 "255.255.0.0": "/16",
+                 "255.254.0.0": "/15",
+                 "255.252.0.0": "/14",
+                 "255.248.0.0": "/13",
+                 "255.240.0.0": "/12",
+                 "255.224.0.0": "/11",
+                 "255.192.0.0": "/10",
+                 "255.128.0.0": "/9",
+                 "255.0.0.0": "/8",
+                 "254.0.0.0": "/7",
+                 "252.0.0.0": "/6",
+                 "248.0.0.0": "/5",
+                 "240.0.0.0": "/4",
+                 "224.0.0.0": "/3",
+                 "192.0.0.0": "/2",
+                 "128.0.0.0": "/1",
+                 "0.0.0.0": "/0"
+                 }
+    mask_bit = dicc_mask.get(mask)
+    return mask_bit
+
+
 # creo archivo con la configuracion vieja del cliente
 lista_almacen_configs = []
 
@@ -59,7 +98,6 @@ for linea_vlan in lista_vlan_vieja:
             last_oct = int(octecs_ip[3]) + 1
             next_hop = octecs_ip[0] + "." + octecs_ip[1] + "." + octecs_ip[2] + "." + str(last_oct)
 
-            
         elif marcar_int == 1 and "service-policy input" in linea_router_config:
             almacen_router_config = almacen_router_config + linea_router_config
         elif marcar_int == 1 and "service-policy output" in linea_router_config:
@@ -70,7 +108,7 @@ for linea_vlan in lista_vlan_vieja:
             for linea_router_router_config2 in lista_router_config:
                 if "ip route" in linea_router_router_config2 and\
                      next_hop in linea_router_router_config2:
-                    almacen_route = almacen_route + linea_router_router_config2
+                    almacen_route = almacen_route + "no " + linea_router_router_config2
             lista_almacen_configs.append(almacen_router_config + "exit\n" + almacen_route)
             marcar_int = 0
 
@@ -112,6 +150,15 @@ for index_lista in range(size_lista):
                 if "encapsulation dot1Q" in item_interface:
                     lista_dot1q = item_interface.split(" ")
                     item_interface = "encapsulation dot1Q " + lista_vlan_nueva[index_lista][:-1]
+                elif "ip address" in item_interface:
+                    list_item_interface = item_interface.split(" ")
+                    ip_address = list_item_interface[3]
+                    mask_dec = list_item_interface[4]
+                    
+                    mask_bit = convertir_mask(mask_dec)
+
+                    IP_ADDRESS_BIT = ip_address + mask_bit
+
                 elif "ip route" in item_interface:
                     if "RED1" in item_interface:
                         item_interface = item_interface.replace("RED1", "GPON")
@@ -120,8 +167,22 @@ for index_lista in range(size_lista):
                 almacen_interface = almacen_interface + item_interface + "\n"
         if "DATOS_INTERFACE_NUEVA" in linea_plantilla_config:
             linea_plantilla_config = almacen_interface[:-1]
-
-        lista_salida.append(linea_plantilla_config)    
+        if "IP_ADDRESS_BIT" in linea_plantilla_config:
+            linea_plantilla_config = linea_plantilla_config.replace("IP_ADDRESS_BIT", IP_ADDRESS_BIT)
+        if "RUTAS_ESTATICAS_JUN" in linea_plantilla_config:
+            rutas_jun = ""
+            if "ip route" in almacen_interface:
+                list_almacen_interface = almacen_interface.split("\n")
+                for item_almacen_interface in list_almacen_interface:
+                    if "ip route" in item_almacen_interface:
+                        list_item_almacen_interface = item_almacen_interface.split(" ")
+                        mask_bit = convertir_mask(list_item_almacen_interface[4])
+                        rutas_jun = rutas_jun + "set routing-options static route " + list_item_almacen_interface[3] + mask_bit + " next-hop " + list_item_almacen_interface[5] + "\n"
+                linea_plantilla_config = linea_plantilla_config.replace("RUTAS_ESTATICAS_JUN", rutas_jun)
+            else:
+                linea_plantilla_config = linea_plantilla_config.replace("RUTAS_ESTATICAS_JUN", "NO TIENE ESTATICAS")
+            
+        lista_salida.append(linea_plantilla_config)  
     string_salida = ""
     for item in lista_salida:
         string_salida = string_salida + item
