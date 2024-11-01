de24386ae753ac971981add6373aa763622aa65f
jrmr100 <jrmr100@gmail.com>
2024-11-01 14:34:42 -0400

    doc: arrglos residuos BOL

--- datos.py
+++ datos.py
@@ -1,9 +1,12 @@
-working_directory = "migracion_GPON-BOL-TO-LEB1006-301024/"
+# DINAMICA
+working_directory = "migracion_GPON-LIB-TO-LEB1006-041124/"
+interface_anterior = "TenGigabitEthernet1/0/2."
+interface_nueva = "Port-channel11"
+
+# ESTATICA
 txt_vlan_vieja = working_directory + "lista_vlan_vieja.txt"
 txt_vlan_nueva = working_directory + "lista_vlan_nueva.txt"
 txt_nombres_vlan = working_directory + "lista_nombres_vlan.txt"
 txt_salida = working_directory + "salida/salida-"
 txt_plantilla_config = working_directory + "plantilla_config.txt"
 txt_router_config = working_directory + "router_config.txt"
-interface_anterior = "interface Port-channel4."
-interface_nueva = "Port-channel10"
